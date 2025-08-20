d50f34008b8f31da2f9861dd03edb540f584d915
Malin Elser <malin.elser@volvo.com>
2025-08-20 16:07:06 +0200

    disabled undotree and enabled built in undo plugin from snacks

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+Not quite understood the difference between the undo tree plugin and the undo history plugin included in snacks.nvim, and also unclear why it wasn't there until I pasted in all the code from the README on snacks github page into snacks.lua
+Anyhow, now I have commented out all code from undotree.lua, meaning disabled the undo tree plugin.
--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,7 +1,7 @@
-return {
-  "mbbill/undotree", -- GitHub repo of the plugin
-  cmd = "UndotreeToggle", -- Lazy-loads when this command is used
-  keys = {
-    { "<leader>uu", "<cmd>UndotreeToggle<cr>", desc = "Toggle UndoTree" }, -- Maps <leader>uu to open the UI
-  },
-}
+-- return {
+--   "mbbill/undotree", -- GitHub repo of the plugin
+--   cmd = "UndotreeToggle", -- Lazy-loads when this command is used
+--   -- keys = {
+--     -- { "<leader>uu", "<cmd>UndotreeToggle<cr>", desc = "Toggle UndoTree" }, -- Maps <leader>uu to open the UI
+--   -- },
+-- }
