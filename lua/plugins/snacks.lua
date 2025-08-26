4ac93c6b748cc418f920ff8159739f9a3dcba90a
Malin Elser <malin.elser@volvo.com>
2025-08-26 15:24:46 +0200

    added preview scroll

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,33 +3,53 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
+        formatters = {
+          file = {
+            filename_first = false, -- display filename before the file path
+            truncate = 999, -- truncate the file path to (roughly) this length
+            filename_only = false, -- only show the filename
+            icon_width = 2, -- width of the icon (in characters)
+            git_status_hl = true, -- use the git status highlight group for the filename
+          },
+        },
         sources = {
           grep = {
-            hidden = true, -- search dotfiles
-            ignored = true, -- also search ignored files (optional)
-            -- you can also exclude patterns if needed:
-            -- exclude = { ".git", "node_modules", ".venv" },
+            hidden = true, -- include dotfiles
+            ignored = true, -- include ignored files
           },
           files = {
-            hidden = true, -- include dotfiles
-            ignored = true, -- include ignored (from .gitignore, etc.)
-            -- exclude specific dirs if you want
-            -- exclude = { ".git", "node_modules", ".venv" },
+            hidden = true,
+            ignored = true,
           },
           explorer = {
             hidden = true,
             ignored = true,
-            -- you can also add exclude patterns here if needed
+          },
+        },
+        win = {
+          input = {
+            keys = {
+              -- custom scroll mappings (Alt + Arrows)
+              ["<C-Right>"] = { "preview_scroll_right", mode = { "n", "i" } },
+              ["<C-Left>"] = { "preview_scroll_left", mode = { "n", "i" } },
+              ["<C-Down>"] = { "preview_scroll_down", mode = { "n", "i" } },
+              ["<C-Up>"] = { "preview_scroll_up", mode = { "n", "i" } },
+
+              ["C-x"] = { "list_scroll_right", mode = { "n", "i" } },
+              ["<M-Left>"] = { "list_scroll_left", mode = { "n", "i" } },
+              ["<M-Down>"] = { "list_scroll_down", mode = { "n", "i" } },
+              ["<M-Up>"] = { "list_scroll_up", mode = { "n", "i" } },
+            },
           },
         },
       },
-      scroll = { enabled = true },
-      notifier = { enabled = true },
-      statuscolumn = { enabled = true },
-      input = { enabled = true },
-      quickfix = { enabled = true },
-      hover = { enabled = true },
-      -- add any new Snacks modules here as they’re introduced
+      -- enable other modules here
+      -- scroll = { enabled = true },
+      -- notifier = { enabled = true },
+      -- statuscolumn = { enabled = true },
+      -- input = { enabled = true },
+      -- quickfix = { enabled = true },
+      -- hover = { enabled = true },
     },
   },
 }
